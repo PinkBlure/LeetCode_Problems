06d650d6c2b3e5b9f402dcea8cdda9b3790cbde2
Aileen <aileenpadrontorres511@gmail.com>
2023-09-06 12:45:42 +0100

    Add Palindrome_Number

---
+++ Typescript/dist/Palindrome_Number.js
@@ -0,0 +1,12 @@
+"use strict";
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+const x = 121;
+function isPalindrome(x) {
+    const x_string = x.toString();
+    let i = 0, j = x_string.length - 1;
+    while ((j-- >= i++))
+        if (x_string[i] !== x_string[j])
+            return false;
+    return true;
+}
+console.log(isPalindrome(2442));
