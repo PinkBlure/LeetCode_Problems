1b490e2275f0b0f3f157adca503b17d3aadea092
Aileen <aileenpadrontorres511@gmail.com>
2023-09-07 11:49:50 +0100

    Correct use of an initialized map

--- Typescript/dist/Roman_To_Integer.js
+++ Typescript/dist/Roman_To_Integer.js
@@ -22,14 +22,15 @@
 //     C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
 function romanToInt(s) {
-    const romanMap = new Map();
-    romanMap.set('I', 1);
-    romanMap.set('V', 5);
-    romanMap.set('X', 10);
-    romanMap.set('L', 50);
-    romanMap.set('C', 100);
-    romanMap.set('D', 500);
-    romanMap.set('M', 1000);
+    const romanMap = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+    ]);
     let value = 0;
     let before = 0;
     for (let i = 0; i < s.length; i++) {
--- Typescript/src/Roman_To_Integer.ts
+++ Typescript/src/Roman_To_Integer.ts
@@ -28,14 +28,16 @@
 
 function romanToInt(s: string): number {
 
-    const romanMap = new Map<string, number>()
-    romanMap.set('I', 1)
-    romanMap.set('V', 5)
-    romanMap.set('X', 10)
-    romanMap.set('L', 50)
-    romanMap.set('C', 100)
-    romanMap.set('D', 500)
-    romanMap.set('M', 1000)
+    const romanMap = new Map<string, number>([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+    ])
+    
 
     let value = 0
     let before = 0
